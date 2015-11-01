7aa7d275ede91726024994ef8c78f678f7182de5
Emily Landi <emily.landi@gmail.com>
2015-11-01 13:58:55 -0800

    working on test. can get mocha to run or converter.js to run properly but can't get both to run together.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var fs = require('fs');
 var bitmap1 = require(__dirname + '/../lib/bitmap1');
-
 var converter = require(__dirname + '/lib/converter');
-converter();
--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ var expect = require('chai').expect;
 var converter = require(__dirname + '/../lib/converter');
 var bitmap1 = require(__dirname + '/../lib/bitmap1');
 
-describe('the converter', function() {
-  it('should visually show you your mood', function() {
+describe('the create palette function', function() {
+  it('should change the colors', function() {
     console.log('test');
   })
 })
