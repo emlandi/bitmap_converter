d345bc26ee4c56b116f6423fd18660afc279cd68
Jack Sneed <sneed.jack@gmail.com>
2015-10-31 17:50:27 -0700

    add gulpfile

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('mocha').expect;
+
+var converter = require(__dirname + '/lib/converter');
+
+describe('the converter', function() {
+  it('should visually show you your mood', function() {
+    expect()
+  })
+})
