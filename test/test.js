584762810ba69302c23e025095d0db71115f0395
Emily Landi <emily.landi@gmail.com>
2015-10-31 17:51:17 -0700

    working on test file

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var converter = require(__dirname + '/lib/converter');
+
+describe('the converter', function(){
+  it('should change the color of bitmap', function(){
+    // expect(palette_size).to.not.eql(new_palette);
+    console.log('test');
+  });
+});
